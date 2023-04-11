5ae6e58d482c0148befff54e841f5d1c043ffbf2
Cletus Dsouza <60503321+claydsouza99@users.noreply.github.com>
2023-04-11 22:51:03 +0530

    Add files via upload

---
+++ Module 2 - OOPJ/Assignment 3/Q2/src/org/assignment/three/Program.java
@@ -0,0 +1,55 @@
+package org.assignment.three;
+
+import java.util.Arrays;
+//import java.util.Arrays;
+import java.util.Scanner;
+
+public class Program {
+	
+	static Scanner sc = new Scanner(System.in);
+	
+	private static int menuList() {
+		
+		System.out.println("1. View menu with prices ");
+		System.out.println("2. Add new dish to menu ");
+		System.out.println("3. Remove dish from menu");
+		System.out.println("4. Modify price of a dish");
+		System.out.println("5. Exit");
+		System.out.print("Enter choice: ");
+	
+		return sc.nextInt();
+	}
+	
+
+	public static void main(String[] args) {
+		
+		Restuarant[] arr = RestuarantTest.getArray();
+		
+		RestuarantTest test = new RestuarantTest();
+		
+		System.out.println(arr[0].getDish());
+		
+		
+		int choice;
+		
+		while((choice = Program.menuList() ) != 5) {
+			
+			switch (choice) {
+			case 1:	System.out.println(Arrays.toString(arr));
+				break;
+				
+			case 2: test.AddNewDish();
+				break;
+				
+			case 3: test.RemoveDish();
+				break;
+			
+			case 4: test.ModifyPrice();
+				break;
+			}
+			
+		}
+	}
+		
+	}
+
